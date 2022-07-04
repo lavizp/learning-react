2363b7b09f164fe62d739e10733c4aea68dbea55
Laviz-pand <pandeylaviz@gmail.com>
2022-07-04 21:02:06 +0545

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef} from 'react';
+import TodoList from './TodoList';
+import './styles.css'
 
-function App() {
+function App() { 
+  const[todos, setTodos] = useState([])  
+  const[key,setKey] = useState(() =>{
+    return 0;
+  });           //object destructuring
+  const todoNameRef =useRef();
+  
+  function handleAddTodo(e) {
+    
+    const name = todoNameRef.current.value;
+    if(name ==='') return;
+    todoNameRef.current.value = null;
+    setTodos(prevTodos =>{
+   
+      return [...prevTodos,{id:key, name:name, complete: false}]
+      
+    })
+    setKey(prevKey => prevKey +1);
+  }
+  function toggleTodo(id)
+  {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+  function clearTodo()
+  {
+    const newTodos = todos.filter(todo => !todo.complete);
+    setTodos(newTodos);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input ref={todoNameRef}type = "text"></input>
+      <button onClick={handleAddTodo} className="button">Add Todo</button>
+      <button onClick={clearTodo} className="button">Clear Todo</button>
+      <div className="left-to-do">{todos.filter(todo => !todo.complete).length} Left to do</div>
+    </>
+  )
 }
+
+
 
 export default App;
