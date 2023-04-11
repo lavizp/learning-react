7a996c34eb19d9e6b8572c4341a46980316d370a
Laviz-pand <pandeylaviz@gmail.com>
2023-04-11 09:42:14 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, {useState, useRef} from 'react';
-import ImageContainer from './ImageContainer';
-import OtherMovies from './OtherMovies';
+
 
 function App() { 
  
   return (
     <>
-      <ImageContainer/>
-     <OtherMovies/>
+    asd
     </>
   )
 }
